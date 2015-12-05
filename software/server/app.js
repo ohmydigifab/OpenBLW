af2c489a4a6e9fbd2ec9559a963e1c40317bc829
takumadx <hirayama.takuma@ohmydigifab.com>
2015-12-06 00:16:53 +0900

    openpilot

--- software/server/app.js
+++ software/server/app.js
@@ -1,61 +1,89 @@
 process.chdir(__dirname);
 
+var OpenPilot = require('./openpilot.js');
 var child_process = require('child_process');
-child_process.exec('sh ./sh/init_servo.sh');
-
+var async = require('async');
 var fs = require("fs");
-var server = require("http").createServer(function(req, res) {
-     res.writeHead(200, {"Content-Type":"text/html"});
-     var output = fs.readFileSync("./www/index.html", "utf-8");
-     res.end(output);
-}).listen(9001);
 
-var io = require("socket.io").listen(server);
-
-var throttle = 0.0;
-var accel_factor = 0.02;
-
-io.sockets.on("connection", function (socket) {
-
-	socket.on("connected", function () {
+var op = new OpenPilot();
+async.waterfall([ function(callback) {//connect to openpilot
+	op.connect(function() {
+		callback(null);
 	});
-	  
-	socket.on("ping", function (time) {
-		socket.emit("pong");
-	});
-	
-	socket.on("accelerate_throttle", function (value, callback) {
-		throttle += accel_factor*value;
-		if(throttle < 0.0)
-		{
+}, function(callback) {//start up websocket server
+	child_process.exec('sh ./sh/init_servo.sh');
+
+	var server = require("http").createServer(function(req, res) {
+	     res.writeHead(200, {"Content-Type":"text/html"});
+	     var output = fs.readFileSync("./www/index.html", "utf-8");
+	     res.end(output);
+	}).listen(9001);
+
+	var io = require("socket.io").listen(server);
+
+	var roll = 0.0;
+	var pitch = 0.0;
+	var yaw = 0.0;
+	var throttle = 0.0;
+	var accel_factor = 0.02;
+
+	io.sockets.on("connection", function (socket) {
+
+		socket.on("connected", function () {
+		});
+		  
+		socket.on("ping", function (time) {
+			socket.emit("pong");
+		});
+		
+		socket.on("accelerate_throttle", function (value, callback) {
+			throttle += accel_factor*value;
+			if(throttle < 0.0)
+			{
+				throttle = 0.0;
+			}
+			else if(throttle > 1.0)
+			{
+				throttle = 1.0;
+			}
+			op.setThrust(throttle, function(){
+				callback();
+			});
+		});
+		
+		socket.on("setArm", function (bln, callback) {
 			throttle = 0.0;
-		}
-		else if(throttle > 1.0)
-		{
-			throttle = 1.0;
-		}
-		var cmd = 'sh ./sh/set_throttle.sh ' + Math.round(1000+throttle*1000);
-		child_process.exec(cmd);
-		socket.emit("msg", cmd);
-		callback(throttle);
-	});
-	
-	socket.on("arm", function (callback) {
-		throttle = 0.0;
-		var cmd = 'sh ./sh/arm.sh';
-		child_process.exec(cmd);
-		socket.emit("msg", cmd);
-		callback();
-	});
-	
-	socket.on("disarm", function (callback) {
-		throttle = 0.0;
-		var cmd = 'sh ./sh/disarm.sh';
-		child_process.exec(cmd);
-		socket.emit("msg", cmd);
-		callback();
-	});
-	
-	 socket.on("disconnect", function () {
-	 });
+			op.setArm(bln, function(){
+				callback();
+			});
+		});
+		
+		socket.on("setRoll", function (value, callback) {
+			op.setRoll(value, function(){
+				callback();
+			});
+		});
+		
+		socket.on("setPitch", function (value, callback) {
+			op.setPitch(value, function(){
+				callback();
+			});
+		});
+		
+		socket.on("setYaw", function (value, callback) {
+			op.setYaw(value, function(){
+				callback();
+			});
+		});
+		
+		socket.on("getAttitude", function (callback) {
+			op.getAttitude(function(obj){
+				callback(obj);
+			});
+		});
+		
+		socket.on("disconnect", function () {
+		});
+	}
+} ], function(err, result) {
 });
---
+++ software/server/openpilot.js
@@ -0,0 +1,161 @@
+var async = require('async');
+var Uavtalk = require("uavtalk");
+var EventEmitter = require('events').EventEmitter;
+var SerialPort = require("serialport").SerialPort;
+
+function OpenPilot(board_type, com_port, definition_path) {
+	var BOARD_TYPE = board_type === undefined ? "cc3d" : board_type;
+	var DEFINITION_PATH = definition_path === undefined ? "./openpilot_definitions" : definition_path;
+	var COM_PORT = com_port === undefined ? "/dev/ttyAMA0" : com_port;
+	
+	var objMan = new Uavtalk.ObjectManager(definition_path);
+	var gtsObj;
+	var ftsObj;
+	
+	var STATUS_DISCONNECTED = 0;
+	var STATUS_HANDSHAKEREQ = 1;
+	var STATUS_HANDSHAKEACK = 2;
+	var STATUS_CONNECTED = 3;
+
+	function getBlankGtsObj() {
+		var gtsObj = {};
+		gtsObj.TxDataRate = 0;
+		gtsObj.TxBytes = 0;
+		gtsObj.TxFailures = 0;
+		gtsObj.TxRetries = 0;
+		gtsObj.RxDataRate = 0;
+		gtsObj.RxBytes = 0;
+		gtsObj.RxFailures = 0;
+		gtsObj.RxSyncErrors = 0;
+		gtsObj.RxCrcErrors = 0;
+		gtsObj.Status = 0;
+		gtsObj.name = "GCSTelemetryStats";
+		gtsObj.object_id = objMan.getObjectId(gtsObj.name);
+		return gtsObj;
+	}
+
+	return {
+		connect : function(callback_connected) {
+			async.waterfall([ function(callback) {
+				objMan.init(function() {
+					callback(null);
+				});
+			}, function(callback) {
+				var sp = new SerialPort(COM_PORT, {
+					baudrate : 57600
+				});
+				objMan.output_stream = function(data) {
+					console.log("data");
+					console.log(data);
+					sp.write(data, function() {
+						sp.drain();
+					});
+				};
+				sp.on("data", objMan.input_stream);
+				sp.on("open", function() {
+					callback(null);
+				});
+			}, function(callback) {
+				objMan.requestObject("FlightTelemetryStats", function(obj) {
+					callback(null, obj);
+				});
+			}, function(obj, callback) {
+				gtsObj = getBlankGtsObj();
+				var connection = function(obj) {
+					ftsObj = obj;
+					console.log(ftsObj);
+					if (ftsObj.Status == STATUS_DISCONNECTED) {
+						gtsObj.Status = STATUS_HANDSHAKEREQ;
+						console.log(gtsObj);
+						objMan.updateObject(gtsObj);
+					} else if (ftsObj.Status == STATUS_HANDSHAKEACK) {
+						gtsObj.Status = STATUS_CONNECTED;
+						console.log(gtsObj);
+						objMan.updateObject(gtsObj);
+					} else if (ftsObj.Status == STATUS_CONNECTED) {
+						console.log("connected");
+						callback(null);
+						return;
+					}
+					objMan.requestObject("FlightTelemetryStats", connection);
+				};
+				connection(obj);
+			}, function(callback) {
+				callback(null);
+			} ], function(err, result) {
+				callback_connected();
+			});
+		},
+		setArm : function(bArm, callback) {
+			objMan.requestObject("FlightModeSettings", function(obj) {
+				if (obj == null || obj.Arming == null) {
+					callback(false);
+					return;
+				}
+				obj.Arming = bArm ? FLIGHTMODESETTINGS_ARMING_YAWLEFT : FLIGHTSTATUS_ARMED_DISARMED;
+				objMan.updateObject(obj);
+				callback(true);
+			});
+		},
+		getArm : function(callback) {
+			objMan.requestObject("FlightStatus", function(obj) {
+				if (obj == null || obj.Armed == null) {
+					callback(false);
+					return;
+				}
+				callback(obj.Armed);
+			});
+		},
+		setRoll : function(value, callback) {
+			objMan.requestObject("ManualControlCommand", function(obj) {
+				if (obj == null) {
+					callback(false);
+					return;
+				}
+				obj.Roll = value;
+				objMan.updateObject(obj);
+				callback(true);
+			});
+		},
+		setPitch : function(value, callback) {
+			objMan.requestObject("ManualControlCommand", function(obj) {
+				if (obj == null) {
+					callback(false);
+					return;
+				}
+				obj.Pitch = value;
+				objMan.updateObject(obj);
+				callback(true);
+			});
+		},
+		setYaw : function(value, callback) {
+			objMan.requestObject("ManualControlCommand", function(obj) {
+				if (obj == null) {
+					callback(false);
+					return;
+				}
+				obj.Yaw = value;
+				objMan.updateObject(obj);
+				callback(true);
+			});
+		},
+		setThrust : function(value, callback) {
+			objMan.requestObject("ManualControlCommand", function(obj) {
+				if (obj == null) {
+					callback(false);
+					return;
+				}
+				obj.Thrust = value;
+				objMan.updateObject(obj);
+				callback(true);
+			});
+		},
+		getAttitude : function(callback) {
+			objMan.requestObject("AttitudeState", function(obj) {
+				callback(obj);
+			});
+		}
+	}
+}
+
+exports = OpenPilot;
